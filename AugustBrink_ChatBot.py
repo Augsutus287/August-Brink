ba1b60c6b1cec56a7eaae787faa35302d9cf3482
Augsutus287 <augustbrink2009@gmail.com>
2026-03-12 14:13:35 +0100

    Add files via upload

--- AugustBrink_ChatBot.py
+++ AugustBrink_ChatBot.py
@@ -1,1 +1,34 @@
+import random
 
+#Hälsningar
+greetings = ["Hello", "hi", "gretings"] 
+greetings_responses = ["Hello if you want to talk am all ears", "Hi do you want to tell me about your day?"]
+
+#Glad
+happy = ["happy", "glad", "good"]
+happy_responses = ["That's great to hear! Anything else on your mind", "I'm glad you're feeling good! Can you tell me more about it", "Keep up the positive vibes!"]
+
+#ledsen 
+sad = ["sad", "down", "unhappy", "unwell"]
+sad_responses = ["I'm sorry to hear that. Do you want to talk about it?", "It's okay to feel sad sometimes. I'm here for you.", "If you want to share what's making you feel down, I'm here to listen."]
+
+#skola
+school = ["school", "homework", "class", "teacher"]
+school_responses = ["School can be tricky sometimes Do you want to talk about what's going on at school?", "Homework can be tough. Do you need to talk about it?", "Teachers can be great or challenging. Do you want to share more about your school experience?"]
+
+#hejdå
+goodbye = [ "bye", "goodbye", "see you later"]
+goodbye_responses = ["Goodbye! Take care!", "See you later! Have a great day!", "Bye! If you ever want to chat again, I'm here!"]
+
+#svordommar
+swear_words = ["damn", "fuck", "shit", "bitch" "ass", "whore", "pussy", "dick", "fuh"]
+swear_words_responses = ["Please watch your language.", "Let's keep the conversation respectful.", "I understand you're upset, but let's try to use kinder words."]
+
+#67
+six_seven = ["67", "six-seven", "sixty seven"]
+six_seven_responses = ["omg six seven", "67 67 67 67 67", "67 is a great number!"]
+
+#fallback resoponses
+fallback_responses = ["I'm not sure I understand. Can you tell me more?", "That's interesting! Can you explain it a bit more?", "I see. Can you elaborate on that?", "sorry i quit you suck at this"]
+
+while True:
